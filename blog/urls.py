bbe88af470130b97c62850e7c3de48bfbece2181
kimbaleum <kimbaleum@likelion.org>
2020-08-04 00:31:16 +0900

    login

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 import blogapp.urls
 import blogapp.views
+import account.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogapp.views.welcome, name='welcome'),
     path('blog/', include('blogapp.urls')),
+    path('account/', include('account.urls')),
 ]
